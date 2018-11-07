c3ae5587b58e831be813cffbaf0fd9f6063ca4f9
LeshardLin <393693197@qq.com>
2018-11-07 14:20:46 +0800

    Initial commit

---
+++ src/main/java/com/jkjk/shiro_imooc/ShiroImoocApplication.java
@@ -0,0 +1,12 @@
+package com.jkjk.shiro_imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroImoocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroImoocApplication.class, args);
+    }
+}
